5756ea68a7473b9b8fb912f768f72d240c1a5b60
rsmitawa <rsmitawa@gmail.com>
2025-01-17 21:09:34 -0500

    Track main.py and crawler.py, add .gitignore

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import chainlit as cl
-from langchain_chroma import Chroma
-from langchain_huggingface import HuggingFaceEmbeddings
-from langchain.chains.retrieval_qa.base import RetrievalQA
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain_community.chains import RetrievalQA
 from langchain_ollama.chat_models import ChatOllama
 from langchain.prompts import PromptTemplate
 import os
